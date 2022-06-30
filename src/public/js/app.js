00e0df6c0162dcfeb234d7a6ce6c7ec8d197d025
Jason <timeli@naver.com>
2022-06-30 23:48:43 +0900

    Feat: 채팅 및 webrtc 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,147 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
+
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+const call = document.getElementById('call');
+const room = document.getElementById('room');
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
+
+room.hidden = true;
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+    cameras.forEach((camera) => {
+      const option = document.createElement('option');
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    await getCameras();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
+
+let roomName;
+let nickName;
+
+function addMessage(message) {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector('#msg input');
+  const value = input.value;
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = '';
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  call.hidden = false;
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName}`;
+  const msgform = room.querySelector('#msg');
+  msgform.addEventListener('submit', handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const $roomName = form.querySelector('#roomName');
+  const $nickName = form.querySelector('#nickName');
+  socket.emit('enter_room', $roomName.value, $nickName.value, showRoom);
+  roomName = $roomName.value;
+  nickName = $nickName.value;
+  $roomName.value = '';
+  $nickName.value = '';
+}
+
+form.addEventListener('submit', handleRoomSubmit);
+
+socket.on('welcome', (user, newCount) => {
+  // 백으로부터 welcome event에 data는 따로 받지 않음, 이벤트가 발생 했을 때 addMessage함수 실행
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName} (${newCount})`;
+  addMessage(`${user} arrived`);
+});
+
+socket.on('bye', (left, newCount) => {
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName} (${newCount})`;
+  addMessage(`${left} left`);
+});
+
+socket.on('new_message', addMessage);
+
+socket.on('room_change', (rooms) => {
+  const roomList = welcome.querySelector('ul');
+  roomList.innerHTML = '';
+  if (rooms.length === 0) {
+    return;
+  }
+  rooms.forEach((room) => {
+    const li = document.createElement('li');
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
 import express from 'express';
 
 const app = express();
@@ -14,14 +14,77 @@ app.get('/*', (req, res) => {
   res.redirect('/');
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
 
-const wss = new WebSocket.Server({ server });
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
 
-wss.on('connection', (socket) => {
-  console.log(socket);
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  console.log(wsServer.sockets.adapter.rooms);
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on('connection', (socket) => {
+  socket['nickname'] = 'Anon';
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on('enter_room', (roomName, nickName, done) => {
+    socket.join(roomName);
+    done(); // 방에 들어가고 done()함수 실행하면 프론트에서 showRoom함수 실행, 프론트에게 방에 들어왔다고 알려줄 수 있다.
+    socket['nickname'] = nickName;
+    console.log(roomName, nickName);
+    socket.to(roomName).emit('Welcome', socket.nickname, countRoom(roomName)); // "welcome" event를 roomName에 있는 모든 사람에게 emit(방에 들어온 당사자를 제외하고)/ 프론트에서 이 emit에 반응하도록 해줘야함
+    wsServer.sockets.emit('room_change', publicRooms()); // 방에 들어왔을 때 방 정보 입력
+  });
+
+  socket.on('disconnecting', () => {
+    // 창을 닫거나 컴퓨터까 꺼졌을 때 방에 보낼 수 있다.
+    socket.rooms.forEach(
+      (room) =>
+        socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1) // 방을 떠나기 직전, 완전히 떠나지 않았으므로 room이름에 접근할 수 있다. 근데 우리도 포함되서 계산됨으로 -1을 해준다.
+    );
+    // socket.rooms는 array같은 중복요소가 없는 set이여서 iterable이 가능
+  });
+
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
+
+// Adapter은 누가 연결되었는지, 현재 어플리케이션에 room이 얼마나 있는지 알려준다.
+// io.socket.adapter을 하면 어플리케이션에 연결된 rooms와 socketid 모든 정보가 나온다.
+// socket은 private room이 있기 때문에 private message를 보낼 수 있는데 그래서 기본 room과 socketid는 같다
+// sids는 백엔드에 연결된 모든 sockets들의 map이다. rooms도 마찬가지
+// 여기서 sids와 rooms를 이용해 public room의 id만 가져올 수 있다.
+// map의 value가 아니라 key에 room id가 있다.
+// rooms.forEach((_, key) => {
+//   if (sids.get(key) === undefined) {
+//     console.log(key);
+//   }
+// });
+// 위와 같이 하면 public room 정보를 가져올 수 있다.
